0466e4695235ce27a74942f61165d752991a0283
Pavlo Semenikhin <pashock1337@gmail.com>
2025-08-21 17:35:51 +0300

    Move SECRET_KEY to .env

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
@@ -8,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t=fmzvc%dqyqh_*6pr9-za$37=pu8%)p%bdpd7o8%ltvm%rjmf"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
