1fdd52c5a37324cf6ec99126c5d68e515f32c9b1
Pavlo Semenikhin <pashock1337@gmail.com>
2025-08-23 20:33:44 +0300

    Add login & register pages with styling and assets

---
+++ accounts/templatetags/form_filters.py
@@ -0,0 +1,8 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name="add_class")
+def add_class(field, css_class):
+    existing_classes = field.field.widget.attrs.get("class", "")
+    return field.as_widget(attrs={"class": f"{existing_classes} {css_class}"})
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
